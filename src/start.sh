53c2fa928a15b69b3f0cfc01fce76a32635e15ff
Shubhankar Das <das.shubhankar97@gmail.com>
2025-03-13 13:18:02 +0530

    changes

--- src/start.sh
+++ src/start.sh
@@ -2,11 +2,10 @@
 set -eo pipefail
 
 # mount file store if fs ip is set
-# if Cloud storage type is S3
 if [ -n "$FS_SHARE" ]; then
   echo "Mounting Cloud Filestore."
 
-  if [ "$CLOUD_STORAGE_TYPE" = "S3" ]; then
+  if [ "$CLOUD_TYPE" = "AWS" ]; then
       mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_SHARE$DATA_PATH $DATA_PATH
       mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_SHARE$MODELS_PATH $MODELS_PATH
 
@@ -16,8 +15,7 @@ if [ -n "$FS_SHARE" ]; then
   fi
 
 
-  # if Cloud storage type is GCS
-  if [ "$CLOUD_STORAGE_TYPE" = "GCS" ]; then
+  if [ "$CLOUD_TYPE" = "GCP" ]; then
       mount -o nolock $FS_SHARE$DATA_PATH $DATA_PATH
       mount -o nolock $FS_SHARE$MODELS_PATH $MODELS_PATH
 
