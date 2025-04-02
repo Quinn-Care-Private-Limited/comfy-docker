029bf4adad37a9c87a94d7cecac6c2048f27e294
Shubhankar Das <das.shubhankar97@gmail.com>
2025-04-02 19:52:11 +0530

    port changes

--- src/start.sh
+++ src/start.sh
@@ -28,7 +28,7 @@ fi
 
 
 echo "worker-comfy: Starting ComfyUI"
-python3 /comfyui/main.py --listen --port 8188 --input-directory /comfyui/data --output-directory /comfyui/data --disable-auto-launch --disable-metadata &
+python3 /comfyui/main.py --listen --port $COMFY_PORT --input-directory /comfyui/data --output-directory /comfyui/data --disable-auto-launch --disable-metadata &
 
 echo "worker-comfy: Starting Handler"
 python3 -u /app/main.py
