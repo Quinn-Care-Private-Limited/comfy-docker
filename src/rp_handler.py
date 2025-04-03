1bb093ef3e8e5e1c7065f2413b84faf770fa2881
Shubhankar Das <das.shubhankar97@gmail.com>
2025-04-04 00:09:44 +0530

    fixes

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -2,6 +2,8 @@
 The main serverless worker module for runpod
 """
 import os
+import requests
+import json
 # src imports
 import comftroller
 import utils 
@@ -19,6 +21,15 @@ def callback(job, data):
     else:   
         utils.log(data)
     
+    if "callback_url" in job["input"]:
+        # send callback to comfyui
+        requests.post(job["input"]["callback_url"], headers={'Content-Type': 'application/json'}, data=json.dumps(data))  
+
+def process_callback(tracker, data):
+    data = utils.safe_parse(data)
+    tracker.update_progress(data)
+    return {"progress": tracker.progress, "status": data}
+    
 def handler(job):
     """
     The main function that handles a job of generating an image.
@@ -32,16 +43,15 @@ def handler(job):
     Returns:
         dict: A dictionary containing either an error message or a success status with generated images.
     """
-    run_id = job["id"]
-    job_input = job["data"]["input"] 
-
+    job_id = job["id"]
+    job_input = job["input"]
 
     # Validate inputs
-    if job_input is None:
-        return utils.error(f"no 'input' property found on job data")
+    if "workflow" not in job_input:
+        return utils.error(f"no 'workflow' property found on job input")
 
     # if workflow is a string then validate will try convert to json
-    workflow = utils.validate_json(job_input)
+    workflow = utils.validate_json(job_input["workflow"])
     # ensure workflow is valid JSON:
     if workflow is None:
         return utils.error(f"'workflow' must be a valid JSON object or JSON-encoded string")
@@ -50,7 +60,7 @@ def handler(job):
 
     tracker = utils.ProgressTracker(workflow)
 
-    update_progress = lambda data: callback(job, {"run_id": run_id, "status": "processing", "data": process_callback(tracker, data)})  
+    update_progress = lambda data: callback(job, {"run_id": job_id, "status": "processing", "data": process_callback(tracker, data)})  
     
     input_files = job_input.get("files", [])
 
@@ -63,7 +73,7 @@ def handler(job):
 
     # if 'run' had an error, then stop job and return error as result
     if outputs.get('error'):
-        callback(job, {"run_id": run_id, "status": "failed", "data": outputs.get('error')})
+        callback(job, {"run_id": job_id, "status": "failed", "data": outputs.get('error')})
         return
 
     # Fetching generated images
@@ -99,10 +109,11 @@ def handler(job):
         utils.log(output_datas)
         utils.log("")
 
-    if "bucket" in job["data"] and "creds" in job["data"]:
-        utils.upload_file_gcs(output_files, job["data"]["bucket"], job["data"]["creds"])
+    if "bucket" in job_input and "creds" in job_input:
+        utils.upload_file_gcs(output_files, job_input["bucket"], job_input["creds"])
 
-    callback(job, {"run_id": run_id, "status": "completed", "data": {"progress": 100, "output": output_files}})
+    callback(job, {"run_id": job_id, "status": "completed", "data": {"progress": 100, "output": output_files}})
+    return output_files
 
 
 
